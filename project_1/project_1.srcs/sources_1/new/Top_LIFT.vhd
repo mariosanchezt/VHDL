fb40195e876c6042bb793104f49f62fb35a7e953
mariosanchezt <mario.sanchez.trigo@alumnos.upm.es>
2025-12-12 18:26:53 +0100

    lab
    Co-Authored-By: JaviSM03 <100871114+JaviSM03@users.noreply.github.com>
    Co-Authored-By: javivaldees <100867220+javivaldees@users.noreply.github.com>

--- project_1/project_1.srcs/sources_1/new/Top_LIFT.vhd
+++ project_1/project_1.srcs/sources_1/new/Top_LIFT.vhd
@@ -1,101 +1,106 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Top_LIFT is -- Primer top sin visualizer 
-Port (
-        CLK    : in  std_logic;
-        rst_n         : in  std_logic;
-        botones_fisicos  : in  std_logic_vector(3 downto 0);
-        motor         : out std_logic_vector(1 downto 0);
-        puerta_abierta: out std_logic;
-        piso_actual   : out std_logic_vector(1 downto 0);
-        piso_destino  : out std_logic_vector(1 downto 0)
-
- );
+entity Top_LIFT is
+    Port (
+        CLK             : in  std_logic;
+        rst_n           : in  std_logic; -- CPU_RESETN (Botón Rojo)
+        botones_fisicos : in  std_logic_vector(3 downto 0);
+        
+        -- SALIDAS FÍSICAS (Conectadas a tu Visualizador)
+        SEG             : out std_logic_vector(6 downto 0);
+        AN              : out std_logic_vector(7 downto 0);
+        LED             : out std_logic_vector(15 downto 0)
+    );
 end Top_LIFT;
 
 architecture Structural of Top_LIFT is
--- Señales internas para los timers ambos
-    signal tick_mover_int : std_logic;
-    signal fin_timer_int  : std_logic;
-    signal botones_clean : std_logic_vector(3 downto 0); --Señal ya clean post edge y sync
 
-        --Declaramos los diferentes componentes a continuacion
+    -- SEÑALES INTERNAS
+    signal s_botones_clean : std_logic_vector(3 downto 0);
+    
+    -- Señales de la FSM
+    signal s_tick_mover    : std_logic;
+    signal s_fin_timer     : std_logic;
+    signal s_motor         : std_logic_vector(1 downto 0);
+    signal s_puerta        : std_logic;
+    signal s_piso_actual   : std_logic_vector(1 downto 0);
+    signal s_piso_destino  : std_logic_vector(1 downto 0);
+    
+    -- Señales visuales
+    signal s_leds_target   : std_logic_vector(3 downto 0);
+    signal s_leds_motor    : std_logic_vector(1 downto 0);
+    signal s_led_puerta    : std_logic;
 
+    -- COMPONENTES (Usamos las versiones de tu compañero para los timers)
+    
     COMPONENT Entradas
-        port (
-         CLK       : in  STD_LOGIC;
-        rst_n       : in  STD_LOGIC; 
-        entrada   : in  STD_LOGIC_VECTOR(3 downto 0);
-        button_in : out STD_LOGIC_VECTOR(3 downto 0)
-        );
+        port ( CLK : in STD_LOGIC; rst_n : in STD_LOGIC; 
+               entrada : in STD_LOGIC_VECTOR(3 downto 0); button_in : out STD_LOGIC_VECTOR(3 downto 0));
     END COMPONENT;
     
-    COMPONENT Timer_Lift
-        port (
-            clk      : in  std_logic;
-            rst_n    : in  std_logic;
-            tick_clk : out std_logic
-        );
+    -- Timer del compañero (Sin enable)
+    COMPONENT Timer_Lift 
+        port ( clk : in std_logic; rst_n : in std_logic; tick_clk : out std_logic );
     END COMPONENT;
--- En ambos timers las señales se llaman igual pero las vamos a asignar a diferentes salidas por asi decirlo ya que  cada una tiene una funcion y un tiempo
-    COMPONENT Timer_Puerta
-        port (
-            clk      : in  std_logic;
-            rst_n    : in  std_logic;
-            tick_clk : out std_logic
-        );
-    END COMPONENT;
--- Para la FSM
-COMPONENT Controlador
-        port (
-            CLK            : in  std_logic;
-            rst_n          : in  std_logic;
-            botones_piso   : in  std_logic_vector(3 downto 0);
-            tick_mover     : in  std_logic;
-            fin_timer      : in  std_logic;
-            motor          : out std_logic_vector(1 downto 0);
-            puerta_abierta : out std_logic;
-            piso_actual    : out std_logic_vector(1 downto 0);
-            piso_destino   : out std_logic_vector(1 downto 0)
-        );
-    END COMPONENT;
-begin
---Una vez declarados los componentes, vamos a instanciar cada uno de elllos
-    inst_Entradas : Entradas
-        PORT MAP (
-            CLK       => CLK,
-            rst_n     => rst_n,
-            entrada   => botones_fisicos,
-            button_in => botones_clean
-        );
 
+    -- Timer del compañero (Sin start)
+    COMPONENT Timer_Puerta
+        port ( clk : in std_logic; rst_n : in std_logic; tick_clk : out std_logic );
+    END COMPONENT;
+
+    COMPONENT Controlador
+        port ( CLK : in std_logic; rst_n : in std_logic; botones_piso : in std_logic_vector(3 downto 0);
+               tick_mover : in std_logic; fin_timer : in std_logic; motor : out std_logic_vector(1 downto 0);
+               puerta_abierta : out std_logic; piso_actual : out std_logic_vector(1 downto 0);
+               piso_destino : out std_logic_vector(1 downto 0));
+    END COMPONENT;
+
+    COMPONENT Visualizador
+        Port ( piso_actual : in STD_LOGIC_VECTOR(1 downto 0); piso_target : in STD_LOGIC_VECTOR(1 downto 0);
+               motor_status : in STD_LOGIC_VECTOR(1 downto 0); puerta_abierta : in STD_LOGIC;
+               seg : out STD_LOGIC_VECTOR(6 downto 0); an : out STD_LOGIC_VECTOR(7 downto 0);
+               leds_piso : out STD_LOGIC_VECTOR(3 downto 0); leds_motor : out STD_LOGIC_VECTOR(1 downto 0);
+               led_puerta : out STD_LOGIC);
+    END COMPONENT;
+
+begin
+
+    -- 1. ENTRADAS
+    inst_Entradas : Entradas
+        PORT MAP ( CLK => CLK, rst_n => rst_n, entrada => botones_fisicos, button_in => s_botones_clean );
+
+    -- 2. TIMERS (Conectados directos, sin lógica de control)
+    -- Al no tener enable, estarán soltando ticks todo el rato.
     inst_Timer_Lift : Timer_Lift
-        PORT MAP (
-            clk      => CLK,
-            rst_n    => rst_n,
-            tick_clk => tick_mover_int -- Este lo instanciamos a la variable interna de mover (Plantas ascensor)
-        );
+        PORT MAP ( clk => CLK, rst_n => rst_n, tick_clk => s_tick_mover );
         
     inst_Timer_Puerta : Timer_Puerta
+        PORT MAP ( clk => CLK, rst_n => rst_n, tick_clk => s_fin_timer );
+
+    -- 3. CONTROLADOR
+    inst_Controlador : Controlador
         PORT MAP (
-            clk      => CLK,
-            rst_n    => rst_n,
-            tick_clk => fin_timer_int --Y este otro al timer asociado a la puerta
-        );
-        
-        inst_Controlador : Controlador
-        PORT MAP (
-            CLK            => CLK,
-            rst_n          => rst_n,
-            botones_piso   => botones_clean, --Le asociamos los botones limpitos ya
-            tick_mover     => tick_mover_int, --Aqui asociamos la variable con su correspondiente interna
-            fin_timer      => fin_timer_int,
-            motor          => motor,
-            puerta_abierta => puerta_abierta,
-            piso_actual    => piso_actual,
-            piso_destino   => piso_destino
+            CLK => CLK, rst_n => rst_n, botones_piso => s_botones_clean,
+            tick_mover => s_tick_mover, fin_timer => s_fin_timer,
+            motor => s_motor, puerta_abierta => s_puerta,
+            piso_actual => s_piso_actual, piso_destino => s_piso_destino
         );
 
+    -- 4. VISUALIZADOR
+    inst_Visualizador : Visualizador
+        PORT MAP (
+            piso_actual => s_piso_actual, piso_target => s_piso_destino,
+            motor_status => s_motor, puerta_abierta => s_puerta,
+            seg => SEG, an => AN,
+            leds_piso => s_leds_target, leds_motor => s_leds_motor, led_puerta => s_led_puerta
+        );
+
+    -- 5. SALIDAS A LA PLACA
+    LED(3 downto 0) <= s_leds_target;
+    LED(5 downto 4) <= "00";
+    LED(7 downto 6) <= s_leds_motor;
+    LED(14 downto 8) <= (others => '0');
+    LED(15) <= s_led_puerta;
 
 end Structural;
