c6364b3f252ef7cf9c6089b8de6b9f3aa29fab12
javivaldees <javier.valdes@alumnos.upm.es>
2025-12-13 11:47:59 +0100

    depuracion
    Co-Authored-By: mariosanchezt <100707996+mariosanchezt@users.noreply.github.com>
    Co-Authored-By: JaviSM03 <100871114+JaviSM03@users.noreply.github.com>

--- project_1/project_1.srcs/sources_1/new/Top_LIFT.vhd
+++ project_1/project_1.srcs/sources_1/new/Top_LIFT.vhd
@@ -7,7 +7,7 @@ entity Top_LIFT is
         rst_n           : in  std_logic; -- CPU_RESETN (Botón Rojo)
         botones_fisicos : in  std_logic_vector(3 downto 0);
         
-        -- SALIDAS FÍSICAS (Conectadas a tu Visualizador)
+        -- SALIDAS FÍSICAS (Conectadas a Visualizador)
         SEG             : out std_logic_vector(6 downto 0);
         AN              : out std_logic_vector(7 downto 0);
         LED             : out std_logic_vector(15 downto 0)
@@ -32,71 +32,104 @@ architecture Structural of Top_LIFT is
     signal s_leds_motor    : std_logic_vector(1 downto 0);
     signal s_led_puerta    : std_logic;
 
-    -- COMPONENTES (Usamos las versiones de tu compañero para los timers)
+    -- COMPONENTES 
     
     COMPONENT Entradas
         port ( CLK : in STD_LOGIC; rst_n : in STD_LOGIC; 
                entrada : in STD_LOGIC_VECTOR(3 downto 0); button_in : out STD_LOGIC_VECTOR(3 downto 0));
     END COMPONENT;
     
-    -- Timer del compañero (Sin enable)
+    
     COMPONENT Timer_Lift 
         port ( clk : in std_logic; rst_n : in std_logic; tick_clk : out std_logic );
     END COMPONENT;
 
-    -- Timer del compañero (Sin start)
+   
     COMPONENT Timer_Puerta
-        port ( clk : in std_logic; rst_n : in std_logic; tick_clk : out std_logic );
+        port (
+         clk : in std_logic; 
+        rst_n : in std_logic;
+         tick_clk : out std_logic );
     END COMPONENT;
 
     COMPONENT Controlador
-        port ( CLK : in std_logic; rst_n : in std_logic; botones_piso : in std_logic_vector(3 downto 0);
-               tick_mover : in std_logic; fin_timer : in std_logic; motor : out std_logic_vector(1 downto 0);
-               puerta_abierta : out std_logic; piso_actual : out std_logic_vector(1 downto 0);
-               piso_destino : out std_logic_vector(1 downto 0));
+        port ( 
+        CLK : in std_logic; 
+        rst_n : in std_logic; 
+        botones_piso : in std_logic_vector(3 downto 0);
+        tick_mover : in std_logic; 
+        fin_timer : in std_logic; 
+        motor : out std_logic_vector(1 downto 0);
+       puerta_abierta : out std_logic; 
+       piso_actual : out std_logic_vector(1 downto 0);
+        piso_destino : out std_logic_vector(1 downto 0));
     END COMPONENT;
 
     COMPONENT Visualizador
-        Port ( piso_actual : in STD_LOGIC_VECTOR(1 downto 0); piso_target : in STD_LOGIC_VECTOR(1 downto 0);
-               motor_status : in STD_LOGIC_VECTOR(1 downto 0); puerta_abierta : in STD_LOGIC;
-               seg : out STD_LOGIC_VECTOR(6 downto 0); an : out STD_LOGIC_VECTOR(7 downto 0);
-               leds_piso : out STD_LOGIC_VECTOR(3 downto 0); leds_motor : out STD_LOGIC_VECTOR(1 downto 0);
+        Port ( 
+        piso_actual : in STD_LOGIC_VECTOR(1 downto 0); 
+        piso_target : in STD_LOGIC_VECTOR(1 downto 0);
+               motor_status : in STD_LOGIC_VECTOR(1 downto 0); 
+               puerta_abierta : in STD_LOGIC;
+               seg : out STD_LOGIC_VECTOR(6 downto 0); 
+               an : out STD_LOGIC_VECTOR(7 downto 0);
+               leds_piso : out STD_LOGIC_VECTOR(3 downto 0); 
+               leds_motor : out STD_LOGIC_VECTOR(1 downto 0);
                led_puerta : out STD_LOGIC);
     END COMPONENT;
 
 begin
 
-    -- 1. ENTRADAS
+    -- ENTRADAS
     inst_Entradas : Entradas
-        PORT MAP ( CLK => CLK, rst_n => rst_n, entrada => botones_fisicos, button_in => s_botones_clean );
+        PORT MAP ( CLK => CLK, 
+        rst_n => rst_n, 
+        entrada => botones_fisicos, 
+        button_in => s_botones_clean );
 
-    -- 2. TIMERS (Conectados directos, sin lógica de control)
-    -- Al no tener enable, estarán soltando ticks todo el rato.
+    --  TIMERS 
+    
     inst_Timer_Lift : Timer_Lift
-        PORT MAP ( clk => CLK, rst_n => rst_n, tick_clk => s_tick_mover );
+        PORT MAP ( 
+        clk => CLK, 
+        rst_n => rst_n, 
+        tick_clk => s_tick_mover );
         
     inst_Timer_Puerta : Timer_Puerta
-        PORT MAP ( clk => CLK, rst_n => rst_n, tick_clk => s_fin_timer );
+        PORT MAP ( 
+        clk => CLK, 
+        rst_n => rst_n, 
+        tick_clk => s_fin_timer );
 
-    -- 3. CONTROLADOR
+   -- CONTROLADOR
     inst_Controlador : Controlador
         PORT MAP (
-            CLK => CLK, rst_n => rst_n, botones_piso => s_botones_clean,
-            tick_mover => s_tick_mover, fin_timer => s_fin_timer,
-            motor => s_motor, puerta_abierta => s_puerta,
-            piso_actual => s_piso_actual, piso_destino => s_piso_destino
+            CLK => CLK, 
+            rst_n => rst_n, 
+            botones_piso => s_botones_clean,
+            tick_mover => s_tick_mover, 
+            fin_timer => s_fin_timer,
+            motor => s_motor, 
+            puerta_abierta => s_puerta,
+            piso_actual => s_piso_actual, 
+            piso_destino => s_piso_destino
         );
 
-    -- 4. VISUALIZADOR
+    -- VISUALIZADOR
     inst_Visualizador : Visualizador
         PORT MAP (
-            piso_actual => s_piso_actual, piso_target => s_piso_destino,
-            motor_status => s_motor, puerta_abierta => s_puerta,
-            seg => SEG, an => AN,
-            leds_piso => s_leds_target, leds_motor => s_leds_motor, led_puerta => s_led_puerta
+            piso_actual => s_piso_actual, 
+            piso_target => s_piso_destino,
+            motor_status => s_motor,
+             puerta_abierta => s_puerta,
+            seg => SEG,
+             an => AN,
+            leds_piso => s_leds_target, 
+            leds_motor => s_leds_motor, 
+            led_puerta => s_led_puerta
         );
 
-    -- 5. SALIDAS A LA PLACA
+    -- SALIDAS A LA PLACA
     LED(3 downto 0) <= s_leds_target;
     LED(5 downto 4) <= "00";
     LED(7 downto 6) <= s_leds_motor;
